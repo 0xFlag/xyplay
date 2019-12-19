5e57c3ad156322e196cdf5d540766c8c829795fc
0xFlag <54313514+0xFlag@users.noreply.github.com>
2019-12-19 19:00:19 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/xyplay/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.xyplay;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private FrameLayout fullVideo;
+    private View customView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView=findViewById(R.id.webView);
+        fullVideo=findViewById(R.id.full_video);
+
+        webView.loadUrl("localhost"); //网址
+
+        webView.setWebChromeClient(new MyWebChromeClient());
+        webView.setWebViewClient(new MyWebViewClient());
+        setWebView();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (webView!=null){
+            webView.destroy();
+        }
+    }
+
+    private void setWebView(){
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    private class MyWebChromeClient extends WebChromeClient {
+
+        @Override
+        public void onHideCustomView() {
+
+            if (customView == null){
+                return;
+            }
+            fullVideo.removeView(customView);
+            fullVideo.setVisibility(View.GONE);
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);//清除全屏
+
+        }
+
+        @Override
+        public void onShowCustomView(View view, CustomViewCallback callback) {
+            customView = view;
+            fullVideo.setVisibility(View.VISIBLE);
+            fullVideo.addView(customView);
+            fullVideo.bringToFront();
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//设置全屏
+        }
+
+    }
+
+    private class MyWebViewClient extends WebViewClient{
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            //progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            //progressBar.setVisibility(View.GONE);
+        }
+    }
+}
